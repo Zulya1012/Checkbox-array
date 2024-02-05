4ab7490aea04a9f693095439b1e19999b235eaf3
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-02-05 15:45:11 +0400

    Update flajok.java

--- flajok.java
+++ flajok.java
@@ -1,18 +1,24 @@
 import java.util.Scanner;
-public class flajok{
-	public static void main (String[] arg){
-		int checkbox=0;
-		String username []={"One", "Two", "Three", "Four", "Five"};
-		Scanner s=new Scanner(System.in);
-		String number=s.nextLine();
-		for (int i=0; i<username.length; i++){
-			if (username[i].equals(number)){
-				checkbox=1;
-				System.out.println("This number is in the array");
-				break;
-			}
-		}
-		if (checkbox==0)
-			System.out.println("This number isn't in the array");
-	}
+public class flajok {
+  public static void main(String[] arg) {
+    int checkbox = 0;
+    String username[] = {
+      "One",
+      "Two",
+      "Three",
+      "Four",
+      "Five"
+    };
+    Scanner s = new Scanner(System.in);
+    String number = s.nextLine();
+    for (int i = 0; i < username.length; i++) {
+      if (username[i].equals(number)) {
+        checkbox = 1;
+        System.out.println("This number is in the array");
+        break;
+      }
+    }
+    if (checkbox == 0)
+      System.out.println("This number isn't in the array");
+  }
 }
